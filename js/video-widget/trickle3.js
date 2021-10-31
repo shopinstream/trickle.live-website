1409bbc635062d4defcd47052c263d5a318af72f
Prabhat <prabhat.kumar@zupee.in>
2021-10-31 12:48:03 +0530

    m3u8 url

--- js/video-widget/trickle3.js
+++ js/video-widget/trickle3.js
@@ -14,22 +14,23 @@ document.body.appendChild(div);
 let prefix_local = "./rct_build/static/js";
 let prefix_trickle = "https://cdn.jsdelivr.net/gh/shopinstream/trickle.live-website@main/js/video-widget";
 
-document.querySelector('head').innerHTML += `<link rel="stylesheet" href="${prefix_trickle+'/main.679dad83.chunk.css'}" type="text/css"/>`;
+document.querySelector('head').innerHTML += `<link rel="stylesheet" href="${prefix_trickle+'/2.ab75b8d7.chunk.css'}" type="text/css"/>`;
+document.querySelector('head').innerHTML += `<link rel="stylesheet" href="${prefix_trickle+'/main.b5d220ca.chunk.css'}" type="text/css"/>`;
 
 
 var trickle_app_script = document.createElement('script');
-trickle_app_script.setAttribute('src', prefix_trickle + "/2.a99ae0b0.chunk.js");
+trickle_app_script.setAttribute('src', prefix_trickle + "/2.094da09d.chunk.js");
 document.body.appendChild(trickle_app_script);
 
 var trickle_app_script2 = document.createElement('script');
-trickle_app_script2.setAttribute('src', prefix_trickle + "/3.72a0d38f.chunk.js");
+trickle_app_script2.setAttribute('src', prefix_trickle + "/3.436e1a20.chunk.js");
 document.body.appendChild(trickle_app_script2);
 
 
 var trickle_runtime = document.createElement('script');
-trickle_runtime.setAttribute('src', prefix_trickle + "/runtime-main.38330eb0.js");
+trickle_runtime.setAttribute('src', prefix_trickle + "/runtime-main.bb11c33c.js");
 document.body.appendChild(trickle_runtime);
 
 var trickle_main = document.createElement('script');
-trickle_main.setAttribute('src', prefix_trickle + "/main.fa6f45ea.chunk.js");
+trickle_main.setAttribute('src', prefix_trickle + "/main.8085de90.chunk.js");
 document.body.appendChild(trickle_main);
