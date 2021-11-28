25851bb61f415d3127f503d772450c29e92de783
Prabhat <prabhat.kumar@zupee.in>
2021-11-28 23:31:41 +0530

    updated base logic fetch

--- js/trickle.js
+++ js/trickle.js
@@ -2,7 +2,7 @@
 var trickle_app_version_script = document.createElement('script');
 var uid = document.currentScript.getAttribute('uid');
 fetch('https://trickle-dashboard.herokuapp.com/web/client').then((resp) => {
-    trickle_app_version_script.setAttribute('src', resp.uri);
+    trickle_app_version_script.setAttribute('src', resp.url);
     trickle_app_version_script.setAttribute('uid', uid);
     document.body.appendChild(trickle_app_version_script);
 });
