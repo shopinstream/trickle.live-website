99f0800b047e4503e8f8e5a5b4ef6639f1d3045f
Prabhat <prabhat.kumar@zupee.in>
2021-11-28 22:06:43 +0530

    updated js file

---
+++ js/trickle.js
@@ -0,0 +1,8 @@
+
+var trickle_app_version_script = document.createElement('script');
+var uid = document.currentScript.getAttribute('uid');
+fetch('https://trickle-dashboard.herokuapp.com/web/client').then((resp) => {
+    trickle_app_version_script.setAttribute('src', resp.uri);
+    trickle_app_version_script.setAttribute('uid', uid);
+    document.body.appendChild(trickle_app_version_script);
+});
