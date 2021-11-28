16346522228f93e8b17ffb7f199e2191a1420fbf
Prabhat <prabhat.kumar@zupee.in>
2021-11-28 23:49:45 +0530

    updated

--- js/trickle.js
+++ js/trickle.js
@@ -1,8 +1,9 @@
 
 var trickle_app_version_script = document.createElement('script');
 var uid = document.currentScript.getAttribute('uid');
-fetch('https://trickle-dashboard.herokuapp.com/web/client').then((resp) => {
-    trickle_app_version_script.setAttribute('src', resp.url);
+fetch('https://trickle-dashboard.herokuapp.com/web/client').then(response=>response.json())
+.then(data => {
+    trickle_app_version_script.setAttribute('src', data.uri);
     trickle_app_version_script.setAttribute('uid', uid);
     document.body.appendChild(trickle_app_version_script);
-});
+})
