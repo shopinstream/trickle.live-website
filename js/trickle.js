16512547360fc8acd0d6780ae3bee1c360c4f672
Prabhat <prabhat.kumar@zupee.in>
2021-11-29 10:08:06 +0530

    testing cache purge

--- js/trickle.js
+++ js/trickle.js
@@ -1,7 +1,7 @@
 
 var trickle_app_version_script = document.createElement('script');
 var uid = document.currentScript.getAttribute('uid');
-fetch('https://trickle-dashboard.herokuapp.com/web/client').then(response=>response.json())
+fetch('https://trickle-dashboard.herokuapp.com/web/client-2').then(response=>response.json())
 .then(data => {
     trickle_app_version_script.setAttribute('src', data.uri);
     trickle_app_version_script.setAttribute('uid', uid);
