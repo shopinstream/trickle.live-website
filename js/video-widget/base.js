d62104d8a703237c8dfb6c7f77a3a24b5fa80e21
Prabhat <prabhat.kumar@zupee.in>
2021-12-06 19:33:23 +0530

    updated base related path

--- js/video-widget/base.js
+++
@@ -1,9 +0,0 @@
-
-var trickle_app_version_script = document.createElement('script');
-var uid = document.currentScript.getAttribute('uid');
-fetch('https://trickle-dashboard.herokuapp.com/web/client').then(response=>response.json())
-.then(data => {
-    trickle_app_version_script.setAttribute('src', data.uri);
-    trickle_app_version_script.setAttribute('uid', uid);
-    document.body.appendChild(trickle_app_version_script);
-})
